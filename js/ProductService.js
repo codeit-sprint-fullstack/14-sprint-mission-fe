992d17ae07f404bd76a2635f8447addf693ffa9a
aksths <aksths161@gmail.com>
2026-06-09 17:16:05 +0900

    FEAT: ArticleService.js 추가

--- js/ProductService.js
+++ js/ProductService.js
@@ -5,13 +5,13 @@ export async function getProductList( page = 1, pageSize = 10, keyword = "" ) {
     try {
         const response = await fetch (
             `${PRODUCT_URL}?page=${page}&pageSize=${pageSize}&keyword=${keyword}`
-        );
+        )
         if (!response.ok) {
             throw new Error("상품 목록 조회 실패");
         }
         return await response.json();
     } catch (error) {
-        console.log (error);
+        console.error (error);
     }
 };
 
@@ -20,13 +20,13 @@ export async function getProduct(productId) {
     try {
         const response = await fetch (
             `${PRODUCT_URL}/${productId}`
-        );
+        )
         if (!response.ok) {
             throw new Error("상품 조회 실패");
         }
         return await response.json();
     } catch(error) {
-        console.log (error);
+        console.error (error);
     }
 };
 
@@ -41,13 +41,13 @@ export async function createProduct(name, description, price, tags, images) {
                     'Content-Type': 'application/json',
                 },
             }
-        );
+        )
         if (!response.ok) {
             throw new Error("상품 생성 실패");
         }
         return await response.json();
     } catch(error) {
-        console.log (error);
+        console.error (error);
     }
 };
 
@@ -62,29 +62,29 @@ export async function patchProduct(productId, data) {
                     'Content-Type': 'application/json',
                 },
             }
-        );
+        )
         if (!response.ok) {
             throw new Error("상품 수정 실패");
         }
         return await response.json();
     } catch(error) {
-        console.log (error);
+        console.error (error);
     }
 };
 
 // 상품 삭제
-export async function patchProduct(productId) {
+export async function deleteProduct(productId) {
     try {
         const response = await fetch (
             `${PRODUCT_URL}/${productId}`, {
                 method: "DELETE",
             }
-        );
+        )
         if (!response.ok) {
             throw new Error("상품 삭제 실패");
         }
         return await response.json();
     } catch(error) {
-        console.log (error);
+        console.error (error);
     }
 };
