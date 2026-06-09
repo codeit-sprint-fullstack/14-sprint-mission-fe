8fd28b50cd53168f0c82e080520fa910bd1caff6
kangyeon <dbwldhkd01@naver.com>
2026-06-09 19:00:04 +0900

    중복된 이메일 팝업알림, modal.css로 분리

--- js/auth.js
+++ js/auth.js
@@ -1,5 +1,5 @@
 const USER_DATA = [
-  { email: 'codeit1@codeit.com', password: "codeit101!", nickname: "코딩이1" },
+  { email: 'dbwldhkd01@codeit.com', password: "sh413929^^", nickname: "초록거북" },
   { email: 'codeit2@codeit.com', password: "codeit202!", nickname: "코딩이2" },
   { email: 'codeit3@codeit.com', password: "codeit303!", nickname: "코딩이3" },
   { email: 'codeit4@codeit.com', password: "codeit404!", nickname: "코딩이4" },
@@ -8,11 +8,19 @@ const USER_DATA = [
 ];
 
 function registerUser(email, password, nickname) {
+  const users = JSON.parse(localStorage.getItem('users')) || USER_DATA;
+
+  if (users.some(u => u.email === email)) {
+    alert('사용중인 이메일입니다');
+    return false;
+  }
+
   const newUser = { email, password, nickname };
   USER_DATA.push(newUser);
   localStorage.setItem('users', JSON.stringify(USER_DATA));
   console.log('회원가입 완료:', newUser);
   console.log('전체 사용자:', USER_DATA);
+  return true;
 }
 
 function loginUser(email, password) {
