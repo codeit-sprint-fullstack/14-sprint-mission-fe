c75130af47c0442fe8a233871f2ca651c1f33f2e
haeun-chae <gkdms1399@gmail.com>
2026-06-10 16:10:37 +0900

    feat: Product API 구현

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 import ArticleService from "./ArticleService.js";
+import ProductService from "./ProductService.js";
 
 // Article API 테스트
 const articleParams = {
@@ -35,3 +36,59 @@ ArticleService.patchArticle(25, patchedArticleData)
 ArticleService.deleteArticle(25)
   .then((data) => console.log(data))
   .catch((e) => console.log('에러 발생: ', e.message))
+
+// Product API 테스트
+const productParams = {
+  page: 1,
+  pageSize: 10,
+  orderBy: 'recent',
+  keyword: '',
+}
+try {
+  const productList = await ProductService.getProductList(productParams)
+  console.log(productList)
+} catch (e) {
+  console.log('에러 발생: ', e.message)
+}
+
+try {
+  const product = await ProductService.getProduct(4011)
+  console.log(product)
+} catch (e) {
+  console.log('에러 발생: ', e.message)
+}
+
+const createdProductData = {
+  name: '상품 이름',
+  description: 'string',
+  price: 0,
+  tags: [ ],
+  images: [ 'https://example.com/...' ],
+}
+try {
+  const createdProduct = await ProductService.createProduct(createdProductData)
+  console.log(createdProduct)
+} catch (e) {
+  console.log('에러 발생: ', e.message)
+}
+
+const patchedProductData = {
+  name: '상품 이름',
+  description: 'string',
+  price: 0,
+  tags: [],
+  images: ['https://example.com/...']
+}
+try {
+  const patchedProduct = await ProductService.patchProduct(4010, patchedProductData)
+  console.log(patchedProduct)
+} catch (e) {
+  console.log('에러 발생: ', e.message)
+}
+
+try {
+  const deletedProduct = await ProductService.deleteProduct(4010)
+  console.log(deletedProduct)
+} catch (e) {
+  console.log('에러 발생: ', e.message)
+}
