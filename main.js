ced432fe08aca632d48b14d6ac0e7a85bc801ec6
yangjoonwon <yangjoonwon2@gmail.com>
2026-06-10 18:15:05 +0900

    fix: 폰트 삭제 내용 복구

--- main.js
+++ main.js
@@ -1,6 +1,39 @@
 import {getArticleList, getArticle, createArticle, patchArticle, deleteArticle} from './ArticleService.js';
 import {getProductList, getProduct, createProduct, patchProduct, deleteProduct} from './ProductService.js';
 
+
+const emailInput = document.querySelector('#e-mail');
+const emailError = document.querySelector('#email-error');
+
+const passwordInput = document.querySelector('#password');
+const passwordError = document.querySelector('#password-error');
+
+emailInput.addEventListener('focusout', function() {
+    const emailValue = emailInput.value.trim();
+
+    if (emailValue === '') {
+        emailInput.classList.add('is-invalid');
+        emailError.textContent = '값을 입력하십시오.';
+    }else {
+        emailInput.classList.remove('is-invalid');
+        emailError.textContent = '';
+    }
+});
+
+passwordInput.addEventListener('focusout', function() {
+    const passwordValue = passwordInput.value.trim();
+
+    if (passwordValue === '') {
+        passwordInput.classList.add('is-invalid');
+        passwordError.textContent = '값을 입력하십시오.';
+    }else {
+        passwordInput.classList.remove('is-invalid');
+        passwordError.textContent = '';
+    }
+});
+
+    
+
 // createArticle({
 //   image: 'https://codeit.com/...',
 //   content: '게시글 내용입니다.',
