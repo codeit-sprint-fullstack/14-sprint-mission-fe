f3ce9034c13855ae569a760efee5052a14fb4474
DODONBO <todn1996@naver.com>
2026-06-17 15:15:28 +0900

    상품 정렬 버튼 구현

--- src/components/MarketItems.jsx
+++ src/components/MarketItems.jsx
@@ -1,8 +1,34 @@
 import arrowDownImg from '../assets/img/arrow_down.svg';
 import MarketItem from "./MarketItem.jsx";
 import searchIcon from "../assets/img/ic_search.svg";
+import axios from '../utils/axios.js';
+import { useState, useEffect } from "react";
 
-function MarketItems({marketItems}) {
+function MarketItems() {
+    const [marketItems, setMarketItems] = useState([]);
+    const [isOpen, setIsOpen] = useState(false);
+    const [sortType, setSortType] = useState("최신순");
+
+    const handleLoad = async (orderType = 'recent') => {
+        const marketItemsResponse = await axios.get('?pageSize=10&orderBy=' + orderType);
+        setMarketItems(marketItemsResponse.data.list);
+    }
+
+    const handleSelect = (value) => {
+        setSortType(value);
+        if (value === "최신순") {
+            handleLoad();
+        } else {
+            handleLoad('favorite');
+        }
+
+        setIsOpen(false);
+    };
+
+    useEffect(() => {
+            handleLoad();
+        }, []);
+
     return (
         <section className="item_section">
             <div className="section_title">판매 중인 상품
@@ -13,18 +39,21 @@ function MarketItems({marketItems}) {
                     </div>
                     <button type="button">상품 등록하기</button>
                     <div className="select_wrap">
-                        <div className="select_display">
-                            최신순
+                        <div className="select_display" onClick={() => setIsOpen((prev) => !prev)}>
+                            {sortType}
                             <img src={arrowDownImg} alt="필터 목록 버튼" />
                         </div>
-                        <div className="select_btn_wrap">
-                            <div className="select_btn">
-                                최신순
+                        {isOpen && (
+                            <div className="select_btn_wrap">
+                                <div className="select_btn" onClick={() => handleSelect("최신순")}>
+                                    최신순
+                                </div>
+
+                                <div className="select_btn" onClick={() => handleSelect("좋아요순")}>
+                                    좋아요순
+                                </div>
                             </div>
-                            <div className="select_btn">
-                                좋아요순
-                            </div>
-                        </div>
+                        )}
                     </div>
                 </div></div>
             <div className="item_wrap">
