4fca744fcc4abcf7687a91212f304da64fb08445
yangjoonwon <yangjoonwon2@gmail.com>
2026-06-18 16:54:07 +0900

    페이지 네이션 구현

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -9,15 +9,19 @@ function ProductList () {
     const [products, setProducts] = useState([]);
     const [searchText, setSearchText] = useState('');
     const [orderBy, setOrderBy]= useState('recent');
+    const [currentPage, setCurrentPage] = useState('1');
+    const [totalCount, setTotalCount] = useState(0);
+    const [pageGroup, setPageGroup] = useState(1);
     
     useEffect(() => {
     async function getProducts() {
-        const response = await axios.get(`https://panda-market-api.vercel.app/products?page=1&pageSize=10&orderBy=${orderBy}`);
-
+        const response = await axios.get(`https://panda-market-api.vercel.app/products?page=${currentPage}&pageSize=10&orderBy=${orderBy}`);
+          
         setProducts(response.data.list);
+        setTotalCount(response.data.totalCount);
     }
     getProducts();
-    },[orderBy]);
+    },[orderBy, currentPage]);
 
     const filteredProducts = products.filter((product) => {
         return product.name.toLowerCase().includes(
@@ -32,6 +36,23 @@ function ProductList () {
             return new Date(b.createdAt) - new Date(a.createdAt)
         }
     });
+
+    
+    const pageSize = 10;
+    const totalPages = Math.ceil (totalCount/ pageSize);
+
+
+    const pageGroupSize = 5;
+
+    const startPage = (pageGroup - 1) * pageGroupSize + 1;
+    const endPage = Math.min(startPage + pageGroupSize -1, totalPages)
+
+    const pageNumbers = Array.from(
+        { length: endPage - startPage + 1},
+        (_, index) => startPage + index
+    );
+
+    const totalPagesGroups = Math.ceil(totalPages / pageGroupSize);
 
     return(
     <>
@@ -58,11 +79,37 @@ function ProductList () {
         </div>
 
         <div className="items">
-            {sortedProducts.map((product) => {
+            {products.map((product) => {
                 return (
                     <ProductCard key={product.id} product={product} />
                 );
             })}
+        </div>
+
+        <div className="pagination">
+
+            <button className="button" onClick={() => setPageGroup(pageGroup -1)} 
+            disabled={pageGroup === 1}
+            >
+                &lt;
+            </button>
+            {pageNumbers.map((pageNumber) => {
+                return (
+                    <button className= {`button ${currentPage === pageNumber ? "active" : ""}`}
+                     key={pageNumber}
+                     onClick={() => {
+                        setCurrentPage(pageNumber);
+                     }}
+                    >
+                        {pageNumber}
+                    </button>
+                );
+            })}
+            <button className="button" onClick={() => setPageGroup(pageGroup +1)} 
+            disabled={pageGroup === totalPagesGroups}
+            >
+                &gt;
+            </button>
         </div>
     </>
     );
