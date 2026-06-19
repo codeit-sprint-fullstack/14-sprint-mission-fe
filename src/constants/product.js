ca19f4f5629ec794221b26305c89d4261e4f1b21
taewon-w <282109935+taewon-w@users.noreply.github.com>
2026-06-19 15:02:20 +0900

    feat(services): extend getProductList params

---
+++ src/constants/product.js
@@ -0,0 +1,4 @@
+export const PRODUCT_ORDER_BY = {
+  RECENT: 'recent',
+  FAVORITE: 'favorite',
+}
--- src/services/ProductService.js
+++ src/services/ProductService.js
@@ -1,3 +1,5 @@
+import { PRODUCT_ORDER_BY } from "../constants/product"
+
 const BASE_URL = 'https://panda-market-api-crud.vercel.app'
 
 const createQueryString = (params) => {
@@ -29,8 +31,8 @@ const requestProduct = async (path, options = {}) => {
   }
 }
 
-export const getProductList = async (page = 1, pageSize = 10, keyword = '') => {
-  const queryString = createQueryString({ page, pageSize, keyword })
+export const getProductList = async (page = 1, pageSize = 10, keyword = '', orderBy = PRODUCT_ORDER_BY.RECENT) => {
+  const queryString = createQueryString({ page, pageSize, keyword, orderBy })
 
   return await requestProduct(`/products?${queryString}`)
 }
