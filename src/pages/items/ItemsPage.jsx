1c60b69b76f621baaa25b4c39ae6ff9f6222eaa7
taewon-w <282109935+taewon-w@users.noreply.github.com>
2026-06-19 15:23:26 +0900

    feat(items): create items page layout

--- src/pages/items/ItemsPage.jsx
+++ src/pages/items/ItemsPage.jsx
@@ -1,7 +1,22 @@
 import MainLayout from '../../components/layout/MainLayout'
+import './ItemsPage.css'
 
 const ItemsPage = () => {
-  return <MainLayout>ItemsPage</MainLayout>
+  return (
+    <MainLayout>
+      <div className="items-page">
+        <section className="items-section">
+          <h2>베스트 상품</h2>
+          <div className="best-products-grid"></div>
+        </section>
+
+        <section className="items-section">
+          <h2>판매 중인 상품</h2>
+          <div className="all-products-grid"></div>
+        </section>
+      </div>
+    </MainLayout>
+  )
 }
 
 export default ItemsPage
