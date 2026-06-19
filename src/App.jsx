aa8b2343091a11de844bba55a13ef525b54dc71c
taewon-w <282109935+taewon-w@users.noreply.github.com>
2026-06-19 14:13:29 +0900

    feat: add placeholder pages

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import { Route, Routes } from 'react-router-dom'
 import HomePage from './pages/landing/HomePage'
 import LoginPage from './pages/auth/LoginPage'
 import SignupPage from './pages/auth/SignupPage'
+import ItemsPage from './pages/items/ItemsPage'
+import FaqPage from './pages/faq/FaqPage'
+import PrivacyPage from './pages/privacy/PrivacyPage'
 
 function App() {
   return (
@@ -9,6 +12,9 @@ function App() {
       <Route path="/" element={<HomePage />} />
       <Route path="/login" element={<LoginPage />} />
       <Route path="/signup" element={<SignupPage />} />
+      <Route path="/items" element={<ItemsPage />} />
+      <Route path="/faq" element={<FaqPage />} />
+      <Route path="/privacy" element={<PrivacyPage />} />
     </Routes>
   )
 }
