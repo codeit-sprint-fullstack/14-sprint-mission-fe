c453e7040a4ea551917bb12e06eebabdcafdd7e1
DODONBO <todn1996@naver.com>
2026-06-25 11:34:30 +0900

    api 작성

---
+++ backend/app.js
@@ -0,0 +1,61 @@
+// const express = require('express');
+import express from 'express';
+import dotenv from 'dotenv';
+import Product from './models/Product.js';
+import cors from 'cors';
+import mongoose from 'mongoose';
+dotenv.config()
+const DATABASE_URL = process.env.DATABASE_URL;
+
+await mongoose.connect(DATABASE_URL);
+console.log('connected to db');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+const port = 3000
+
+app.get('/items', async (req, res) => {
+    const items = await Product.find();
+
+    res.send(items);
+});
+
+app.get('/items/:id', async (req, res) => {
+    const targetId = req.params.id;
+    const item = await Product.findById(targetId);
+
+    res.send(item);
+});
+
+app.post('/items', async (req, res) => {
+  const newItem = await Product.create(req.body);
+  res.status(201).send(newItem);
+});
+
+app.patch('/items/:id', async (req, res) => {
+  const item = await Product.findById(req.params.id);
+  if (item) {
+    Object.keys(req.body).forEach((key) => {
+      item[key] = req.body[key];
+    });
+    await item.save();
+    res.send(item);
+  } else {
+    res.status(404).send({ message: 'Cannot find given id.' });
+  }
+});
+
+app.delete('/items/:id', async (req, res) => {
+  const item = await Product.findByIdAndDelete(req.params.id);
+  if (item) {
+    res.sendStatus(204);
+  } else {
+    res.status(404).send({ message: 'Cannot find given id.' });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+});
---
+++ backend/data/seedData.js
@@ -0,0 +1,19 @@
+const seedData = [
+    {
+      "name": "멋쟁이 모자",
+      "description": "딥그린 색상의 멋쟁이 모자 ",
+      "price": 9000,
+      "tags": [
+        "모자"
+      ],
+      "images": [
+        "https://sprint-fe-project.s3.ap-northeast-2.amazonaws.com/Sprint_Mission/user/1902/1782193018210/koczot-hat-womens-filcowy-3857804_1920.jpg"
+      ],
+      "ownerId": 1,
+      "favoriteCount": 0,
+      "createdAt": "2026-06-23T05:36:58.465Z",
+      "updatedAt": "2026-06-23T06:32:25.872Z"
+    }
+]
+
+export default seedData;
