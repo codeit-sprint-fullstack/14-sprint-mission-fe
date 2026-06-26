7f5722691b2f65e29f13b38b5c7dec1d39101485
CHOM <lsk2825@gamil.com>
2026-06-26 17:50:54 +0900

    feat: MongoDB CRUD 기능 구현

---
+++ server/index.js
@@ -0,0 +1,23 @@
+import 'dotenv/config'
+import express from 'express'
+import mongoose from 'mongoose'
+import productRoutes from './routes/productRoutes.js'
+
+const app = express()
+const PORT = 3000
+
+app.use(express.json())
+app.use(productRoutes)
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log('MongoDB connected')
+
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`)
+    })
+  })
+  .catch((error) => {
+    console.error('MongoDB connection error:', error)
+  })
---
+++ server/routes/productRoutes.js
@@ -0,0 +1,103 @@
+import express from 'express'
+import Product from '../models/Product.js'
+
+const productRoutes = express.Router()
+
+productRoutes.get('/products', async (req, res) => {
+  const keyword = req.query.keyword || ''
+  const sort = req.query.sort
+  const offset = Number(req.query.offset) || 0
+  const PAGE_SIZE = 10
+  // 조건 흐름을 눈으로 보기 위해 if/else로 작성, 추후 리펙토링 예정
+  const filter = keyword
+    ? {
+        $or: [
+          { name: { $regex: keyword, $options: 'i' } },
+          { description: { $regex: keyword, $options: 'i' } },
+        ],
+      }
+    : {}
+
+  const sortOption = sort === 'recent' ? { createdAt: -1 } : { createdAt: 1 }
+
+  const filteredProducts = await Product.find(filter)
+    .select('name price createdAt')
+    .sort(sortOption)
+    .skip(offset)
+    .limit(PAGE_SIZE)
+
+  res.send(filteredProducts)
+})
+
+productRoutes.get('/products/:id', async (req, res) => {
+  const id = req.params.id
+  const product = await Product.findById(id)
+  if (!product) {
+    res.status(404).send({ message: 'Cannot find given id.' })
+    return
+  }
+  const responseProduct = {
+    id: product.id,
+    name: product.name,
+    description: product.description,
+    price: product.price,
+    tags: product.tags,
+    createdAt: product.createdAt,
+  }
+
+  res.send(responseProduct)
+})
+
+productRoutes.post('/products', async (req, res) => {
+  const { name, description, price, tags } = req.body
+
+  const product = await Product.create({
+    name,
+    description,
+    price,
+    tags,
+  })
+
+  const responseProduct = {
+    id: product.id,
+    name: product.name,
+    description: product.description,
+    price: product.price,
+    tags: product.tags,
+    createdAt: product.createdAt,
+  }
+
+  res.status(201).send(responseProduct)
+})
+
+productRoutes.patch('/products/:id', async (req, res) => {
+  const id = req.params.id
+  const product = await Product.findByIdAndUpdate(id, req.body, { new: true })
+  if (!product) {
+    res.status(404).send({ message: 'Cannot find given id.' })
+  } else {
+    // 추후 유저의 개인정보 같은 정보 과다 제공을 미리 막기 위해 명시적으로 설계
+    const responseProduct = {
+      id: product.id,
+      name: product.name,
+      description: product.description,
+      price: product.price,
+      tags: product.tags,
+      createdAt: product.createdAt,
+      updatedAt: product.updatedAt,
+    }
+    res.send(responseProduct)
+  }
+})
+
+productRoutes.delete('/products/:id', async (req, res) => {
+  const id = req.params.id
+  const product = await Product.findByIdAndDelete(id)
+  if (product) {
+    res.sendStatus(204)
+  } else {
+    res.status(404).send({ message: 'Cannot find given id.' })
+  }
+})
+
+export default productRoutes
