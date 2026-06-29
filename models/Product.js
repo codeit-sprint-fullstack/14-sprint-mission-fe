66783114acd7dc2f31716b155ef2348c46dad3eb
kangyeon <dbwldhkd01@naver.com>
2026-06-29 10:28:34 +0900

    product.js에 _id,createAt,updateAt 필드 추가

--- models/Product.js
+++ models/Product.js
@@ -1,4 +1,9 @@
+let nextId = 1;
+
 export class Product {
+  /** 고유 ID */
+  _id;
+
   /** 상품명  */
   _name;
 
@@ -17,13 +22,26 @@ export class Product {
   /** 찜하기 수 */
   _favoriteCount;
 
+  /** 생성일시 */
+  _createdAt;
+
+  /** 수정일시 */
+  _updatedAt;
+
   constructor(name, description, price, tags, images, favoriteCount) {
+    this._id = nextId++;
     this._name = name;
     this._description = description;
     this._price = price;
     this._tags = Array.from(tags); // 깊은 복사를 통해, 외부의 배열을 통해 내부 배열을 변경할 수 없도록 합니다.
     this._images = Array.from(images);
     this._favoriteCount = favoriteCount;
+    this._createdAt = new Date();
+    this._updatedAt = new Date();
+  }
+
+  getId() {
+    return this._id;
   }
 
   getName() {
@@ -48,6 +66,14 @@ export class Product {
 
   getFavoriteCount() {
     return this._favoriteCount;
+  }
+
+  getCreatedAt() {
+    return this._createdAt;
+  }
+
+  getUpdatedAt() {
+    return this._updatedAt;
   }
 
   favorite() {
