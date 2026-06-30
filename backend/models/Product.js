e17ad4f6284b68d2501e069fe3233ac001addb8b
young <youngsecondbrain@email.com>
2026-06-30 13:02:35 +0900

    스케마 validation 추가(글자수 제한 등)

--- backend/models/Product.js
+++ backend/models/Product.js
@@ -4,22 +4,25 @@ const ProductSchema = new mongoose.Schema(
   {
     // id, name, description, price, tags, createdAt, updatedAt필드
     name: {
-      type: String, 
-      required: true, 
-      maxLength: 30,
+      type: String,
+      required: true,
+      minLenghth: 1,
+      maxLength: 10,
     },
     description: {
       type: String,
-      maxLength: 500,
+      minLenghth: 10,
+      maxLength: 100,
     },
     price: {
-      type: Number, 
-      required: true, 
+      type: Number,
+      required: true,
       min: 0,
       max: 999999999,
     },
     tags: {
       type: String,
+      maxLength: 5
     },
   },
   {
