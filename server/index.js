0cdc4ed383443c7cf293d6d927fcd8217975583c
CHOM <lsk2825@gamil.com>
2026-07-02 21:42:32 +0900

    feat: PostgreSQL 및 Prisma 초기 스키마 작성

--- server/index.js
+++ server/index.js
@@ -1,25 +1,10 @@
-import cors from 'cors'
-import 'dotenv/config'
 import express from 'express'
-import mongoose from 'mongoose'
-import productRoutes from './routes/productRoutes.js'
 
 const app = express()
 const PORT = process.env.PORT || 3000
 
-app.use(cors())
 app.use(express.json())
-app.use(productRoutes)
 
-mongoose
-  .connect(process.env.MONGODB_URI)
-  .then(() => {
-    console.log('MongoDB connected')
-
-    app.listen(PORT, () => {
-      console.log(`Server is running on port ${PORT}`)
-    })
-  })
-  .catch((error) => {
-    console.error('MongoDB connection error:', error)
-  })
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`)
+})
