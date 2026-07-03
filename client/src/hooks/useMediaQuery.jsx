22a89117c8a54b69e948e9c851341ca15ae10d77
haeun-chae <gkdms1399@gmail.com>
2026-07-03 10:50:42 +0900

    fix: 반응형 분기점 수정

--- client/src/hooks/useMediaQuery.jsx
+++ client/src/hooks/useMediaQuery.jsx
@@ -2,18 +2,14 @@ import { useEffect, useState } from "react"
 
 function UseMediaQuery () {
   const [pageSize, setPageSize] = useState(10)
-  const [bestPageSize, setBestPageSize] = useState(4)
 
   const handleResize = () => {
-    if (window.innerWidth <= 480) {
+    if (window.innerWidth <= 375) {
       setPageSize(4)
-      setBestPageSize(1)
-    } else if (window.innerWidth <= 768) {
+    } else if (window.innerWidth <= 744) {
       setPageSize(6)
-      setBestPageSize(2)
     } else {
       setPageSize(10)
-      setBestPageSize(4)
     }
   }
 
@@ -26,7 +22,7 @@ function UseMediaQuery () {
     }
   }, [])
 
-  return { pageSize, bestPageSize }
+  return { pageSize }
 }
 
 export default UseMediaQuery
