b61e48315377aeb491043c472ca702aca62666de
haeun-chae <gkdms1399@gmail.com>
2026-07-04 22:13:40 +0900

    fix: 태그 입력 유효성 검사 개선

--- client/src/components/form/TagInput.jsx
+++ client/src/components/form/TagInput.jsx
@@ -8,15 +8,17 @@ function TagInput({ label, id, tags, setTags, error, ...props }) {
     const [inputValue, setInputValue] = useState('')
 
   const handleEnter = (e) => {
-    if (e.nativeEvent.isComposing) {
-      return
-    }
+    if (e.nativeEvent.isComposing) return
+    if (e.key !== 'Enter') return
+    e.preventDefault()
 
-    if (e.key === 'Enter') {
-      e.preventDefault()
-      setTags([...tags, inputValue])
-      setInputValue('')
-    }
+    const value = inputValue.trim()  // 공백 제거 
+    if(!value) return  // 빈 문자열 추가되지 않도록 처리
+    if (value.length > 5) return  // 5글자 초과 제한 - UX 향상
+    if (tags.includes(value)) return  // 태그 중복 추가 제한
+
+    setTags([...tags, inputValue])
+    setInputValue('')
   }
 
   const handleTagDelete = (deleteIndex) => {
