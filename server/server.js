8cef21837edc0f0f39ec5334dcb6be842719ac59
joowon-shim <king.garzet@gmail.com>
2026-07-06 15:15:08 +0900

    chore: clean up backend migration

--- server/server.js
+++ server/server.js
@@ -39,6 +39,15 @@ app.use((err, req, res, next) => {
     });
   }
 
+  if (
+    err instanceof Prisma.PrismaClientKnownRequestError &&
+    err.code === "P2003"
+  ) {
+    return res.status(404).json({
+      message: "연결할 데이터를 찾을 수 없습니다.",
+    });
+  }
+
   console.error(err);
 
   res.status(500).json({
