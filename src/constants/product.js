cc7be36465b7318017ec7fbeb225a4b8918d0d50
taewon-w <282109935+taewon-w@users.noreply.github.com>
2026-07-06 16:14:36 +0900

    refactor: share product constants

--- src/constants/product.js
+++ src/constants/product.js
@@ -1,12 +1,14 @@
-export const PRODUCT_ORDER_BY = {
-  RECENT: 'recent',
-}
+import { PRODUCT_ORDER_BY } from '../../shared/constants/product.js'
+
+export {
+  PRODUCT_DESCRIPTION_MAX_LENGTH,
+  PRODUCT_DESCRIPTION_MIN_LENGTH,
+  PRODUCT_NAME_MAX_LENGTH,
+  PRODUCT_ORDER_BY,
+  PRODUCT_PRICE_MIN,
+  PRODUCT_TAG_MAX_LENGTH,
+} from '../../shared/constants/product.js'
 
 export const PRODUCT_SORT_OPTIONS = [
   { label: '최신순', value: PRODUCT_ORDER_BY.RECENT },
 ]
-
-export const PRODUCT_NAME_MAX_LENGTH = 10
-export const PRODUCT_DESCRIPTION_MIN_LENGTH = 10
-export const PRODUCT_DESCRIPTION_MAX_LENGTH = 100
-export const PRODUCT_TAG_MAX_LENGTH = 5
