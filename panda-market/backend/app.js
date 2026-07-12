f9181aa576599c4d320711f5d4b01d77e18e7328
윤여진 <yyjinjin111@gmail.com>
2026-07-12 23:07:27 +0900

    [윤여진]스프린트 미션 6

--- panda-market/backend/app.js
+++ panda-market/backend/app.js
@@ -1,19 +1,15 @@
 import dotenv from "dotenv";
 dotenv.config();
-
-import mongoose from "mongoose";
 import express from "express";
-import Product from "./models/Product.js";
 import cors from "cors";
-const DATABASE_URL = process.env.DATABASE_URL;
+import { data } from "react-router-dom";
 const PORT = process.env.PORT;
-
-await mongoose.connect(DATABASE_URL)
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+/* ------ product -------- */
 app.get("/products", async (req, res) => {
     const keyword = req.query.keyword;
     const page = Number(req.query.page)
@@ -24,19 +20,32 @@ app.get("/products", async (req, res) => {
     let products;
     let totalCount;
     if (keyword) {
-        products = await Product.find({
-            "name": { "$regex": keyword }
+        products = await prisma.product.findMany({
+            where:{
+              name:{
+                contains:keyword
+              }
+            },
+            orderBy:{
+              createdAt:"desc"
+            },
+            skip: skipCount,
+            take: pageSize,
         })
-        .skip(skipCount)
-        .limit(pageSize);
-        totalCount = await Product.countDocuments({
-            "name": { "$regex": keyword }
+
+        totalCount = await prisma.product.count({
+            where:{
+              name:{
+                contains:keyword
+              }
+            }
         })
     } else {
-        products = await Product.find()
-            .skip(skipCount)
-            .limit(pageSize);
-        totalCount = await Product.countDocuments()
+        products = await prisma.product.findMany({
+          skip: skipCount,
+          take: pageSize
+        })
+        totalCount = await prisma.product.count()
     }
 
     res.send({
@@ -45,30 +54,162 @@ app.get("/products", async (req, res) => {
     })
 })
 app.post("/products", async (req, res) => {
-    const newProduct = await Product.create(req.body);
-    res.send(newProduct)
+    const newProduct = await prisma.product.create({
+      data:req.body
+    });
+    res.status(201).send(newProduct)
 })
 app.get("/products/:id", async (req, res) => {
     const productId = req.params.id;
-    const product = await Product.findById(productId)
-    res.send(product)
+    const product = await prisma.product.findUnique({
+      where:{
+        id:productId
+      }
+    })
+    res.status(200).send(product)
 })
 app.patch("/products/:id", async (req, res) => {
     const productId = req.params.id;
     const updateData = req.body;
-    const product = await Product.findByIdAndUpdate(
-        productId,
-        updateData, {
-        new: true
-    }
-    )
-    res.send(product)
+    const productPatch = await prisma.product.update({
+      where:{
+        id:productId,
+      },
+       data:updateData
+    })
+    res.status(200).send(productPatch)
 })
 app.delete("/products/:id", async (req, res) => {
     const productId = req.params.id;
-    const updateData = req.body;
-    res.send(product);
+    const productDelete = await prisma.product.delete({
+      where:{
+        id: productId
+      }
+    })
+    res.status(200).send(productDelete);
 })
+
+/* ------ article -------- */
+app.get("/articles", async (req, res) => {
+  const keyword = req.query.keyword;
+  const page = Number(req.query.page);
+  const pageSize = Number(req.query.pageSize);
+  const skipCount = (page - 1) * pageSize;
+
+  let articles;
+  let totalCount;
+
+  if (keyword) {
+    articles = await prisma.article.findMany({
+      where: {
+        OR: [
+          {
+            title: {
+              contains: keyword,
+            },
+          },
+          {
+            content: {
+              contains: keyword,
+            },
+          },
+        ],
+      },
+      orderBy: {
+        createdAt: "desc",
+      },
+      skip: skipCount,
+      take: pageSize,
+    });
+
+    totalCount = await prisma.article.count({
+      where: {
+        OR: [
+          {
+            title: {
+              contains: keyword,
+            },
+          },
+          {
+            content: {
+              contains: keyword,
+            },
+          },
+        ],
+      },
+    });
+  } else {
+    articles = await prisma.article.findMany({
+      orderBy: {
+        createdAt: "desc",
+      },
+      skip: skipCount,
+      take: pageSize,
+    });
+
+    totalCount = await prisma.article.count();
+  }
+
+  res.status(200).send({
+    articles,
+    totalCount,
+  });
+});
+
+
+app.post("/articles", async(req, res)=>{
+  const newArticle = await prisma.article.create({
+    data:req.body
+  });
+  res.status(201).send(newArticle)
+})
+
+app.get("/articles/:id", async(req, res)=>{
+  const articleId = req.params.id;
+  const article = await prisma.article.findUnique({
+    where:{
+      id:articleId
+    }
+  })
+  res.status(200).send(article)
+})
+
+app.patch("articles/:id", async(req, res)=>{
+  const articleId = req.params.id;
+  const updateData =  req.body;
+  const articlePatch = await prisma.article.update({
+    where:{
+      id:articleId
+    },
+    data:updateData
+  })
+  res.status(200).send(articlePatch)
+})
+
+app.delete("/articles/:id", async(req, res)=>{
+  const articleId =  req.params.id;
+  const articleDelete = await prisma.article.delete({
+    where:{
+      id:articleId
+    }
+  })
+  res.status(200).send(articleDelete)
+})
+
+app.post("/products/:productId/comments", async (req, res) => {
+  const productId = req.params.productId;
+  const content = req.body.content;
+
+  const productComment = await prisma.productComment.create({
+    data: {
+      content: content,
+      productId: productId,
+    },
+  });
+  res.status(201).send(productComment);
+});
+
+app.get("/products/:productId/comments")
 
 app.listen(PORT, () => {
     console.log("실행완료");
