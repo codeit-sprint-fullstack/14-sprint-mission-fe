7af5137f29bc347ea958193c70a9cccc58904aaa
young <youngsecondbrain@email.com>
2026-08-03 18:33:17 +0900

    feat: 상세 게시글 페이지 기본틀 구현

--- app/board/[id]/page.js
+++ app/board/[id]/page.js
@@ -1,7 +1,16 @@
-export default function BoardDetail() {
+export default async function ArticleDetail({ params }) {
+  const { id } = await params;
+  const res = await fetch(
+    `https://one4-sprint-mission-prisma.onrender.com/articles/${id}`,
+    { cache: "no-store" },
+  );
+
+  const article = await res.json();
+
   return (
     <div>
-      <h2>게시글 제목</h2>
+      <h1>{article.title}</h1>
+      <span>{article.content}</span>
     </div>
   );
 }
--- app/board/page.js
+++ app/board/page.js
@@ -1,11 +1,25 @@
-import SearchForm from "@/components/SearchForm.js/SearchForm";
+import ArticleList from "@/components/ArticleList";
+import SearchForm from "@/components/SearchForm";
 
-export default function Board() {
+export default async function Board() {
+  const res = await fetch(
+    "https://one4-sprint-mission-prisma.onrender.com/articles",
+    {
+      cache: "no-store",
+    },
+  );
+
+  if (!res.ok) {
+    throw new Error("게시글 목록을 불러오는 데 실패했습니다");
+  }
+
+  const data = await res.json();
   return (
     <div>
       <h2>베스트 게시글</h2>
       <h2>게시글</h2>
       <SearchForm />
+      <ArticleList articles={data} />
     </div>
   );
 }
