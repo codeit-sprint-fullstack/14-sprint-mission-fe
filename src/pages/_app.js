0dab7892e56d4d530b81bcde089b4ddf5a9cb9dc
yangjoonwon <yangjoonwon2@gmail.com>
2026-08-13 09:28:19 +0900

    chore: TanStack React Query 설정

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,15 @@
+import { useState } from "react";
 import Head from "next/head";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
+  const [queryClient] = useState(() => new QueryClient());
+
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <Head>
         <title>판다마켓</title>
         <meta name="description" content="판다마켓 자유게시판" />
@@ -20,6 +24,6 @@ export default function App({ Component, pageProps }) {
 
         <Footer />
       </div>
-    </>
+    </QueryClientProvider>
   );
 }
