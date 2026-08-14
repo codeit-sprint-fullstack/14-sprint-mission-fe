c152834943fb9bcac9b4d73b7717d604520bbbef
CHOM <lsk2825@gamil.com>
2026-08-14 11:27:31 +0900

    refactor: 게시판 기본 이미지 리소스 분리

--- panda-market/src/components/boards/BestArticleCard.jsx
+++ panda-market/src/components/boards/BestArticleCard.jsx
@@ -16,7 +16,7 @@ function BestArticleCard({ article }) {
         {/* article 데이터 모델에 사진, 닉네임, 종아요수 필드이 없으므로 프론트엔드에서 처리 */}
         <div className={styles.bestArticleImage}>
           <Image
-            src="/default_image.png"
+            src="/img_article_default.png"
             alt="제품사진"
             width={48}
             height={44.571}
