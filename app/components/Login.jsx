1b90f46bcc9515e26234011fac4120c297d48e96
shoooongs <shoooongs@gmail.com>
2026-08-16 22:14:16 +0900

    feat: Route Group 분리 + GNB 로그인 상태 분기

---
+++ app/components/Login.jsx
@@ -0,0 +1,30 @@
+"use client";
+
+import profileIc from "@/public/icon_profile.png";
+import { useQuery } from "@tanstack/react-query";
+import Image from "next/image";
+import Link from "next/link";
+import { getMe } from "../lib/api/auth";
+
+export default function Login() {
+  const { data, isPending } = useQuery({
+    queryKey: ["users", "me"],
+    queryFn: getMe,
+  });
+  const showUser = !isPending && data;
+
+  return (
+    <>
+      {showUser ? (
+        <span>
+          <Image src={profileIc} alt="" width={40} />
+          <Link href="/signin">{data?.nickname}</Link>
+        </span>
+      ) : (
+        <Link href="/signin" className="btStyle">
+          로그인
+        </Link>
+      )}
+    </>
+  );
+}
--- app/lib/api/auth.js
+++ app/lib/api/auth.js
@@ -9,3 +9,17 @@ export async function signUp(newUser) {
   const res = await axios.post("/auth/signUp", newUser);
   return res.data;
 }
+
+//유저 인가를 확인하고 정보를 가져오는 함수
+export async function getMe() {
+  try {
+    const res = await axios.get("/users/me");
+    return res.data;
+  } catch (error) {
+    if (error.response?.status === 401) {
+      return null;
+    } else {
+      throw error;
+    }
+  }
+}
